6ee6b6feed7343b5b93c5d3ea4e476e760ad103a
RDF014 <rdf014@gmail.com>
2017-06-25 00:09:14 -0700

    look into better test coverage

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import sum from '../sum';
 // import startServer from '../server/index.js';
 
 const api = axios.create({baseURL: 'http://localhost:1337/'});
@@ -20,6 +21,12 @@ describe('practice MVP server test', () => {
       .post('/Users', {user: 'Raffy'})
       .then(res => res.data.user);
     expect(user).toEqual('Raffy');
+  });
+
+
+  test('adds 1 + 2 to equal 3', () => {
+    const result = sum(1, 2);
+    expect(result).toBe(3);
   });
   
 })
